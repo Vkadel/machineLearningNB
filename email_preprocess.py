86f0d9ae1d5e9e9da63fca614375cb849110bff6
Vkadel <35178154+Vkadel@users.noreply.github.com>
2019-03-27 12:29:41 -0400

    with Loop to change  preprocess Training Size sample as well as  the var_smoothing in the GB

--- email_preprocess.py
+++ email_preprocess.py
@@ -11,7 +11,7 @@ from sklearn.feature_selection import SelectPercentile, f_classif
 
 
 
-def preprocess(words_file = "../tools/word_data_unix.pkl", authors_file="../tools/email_authors.pkl"):
+def preprocess(words_file, authors_file,test_size_number):
     """ 
         this function takes a pre-made list of email texts (by default word_data.pkl)
         and the corresponding authors (by default email_authors.pkl) and performs
@@ -57,7 +57,7 @@ def preprocess(words_file = "../tools/word_data_unix.pkl", authors_file="../tool
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=test_size_number, random_state=42)
 
 
 
--- nb_author_id.py
+++ nb_author_id.py
@@ -12,7 +12,7 @@
     
 import sys
 
-from time import time
+import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -24,19 +24,27 @@ from sklearn.metrics import accuracy_score
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
-
-print(features_test[0])
 
 
 #########################################################
-### your code goes here ###
-loop=[0,1,2,3,4,5,6,7,8,9]
-for i in loop:
-    gnb = GaussianNB(var_smoothing=1e-7)
-    pred = gnb.fit(features_train, labels_train).predict(features_test)
-    acc=accuracy_score(labels_test,pred)
-    print("Accuracy for ",i,": ", acc)
+    ### your code goes here ###
+sample_size_list = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+##Loop to change sample Size
+for j in sample_size_list:
+    features_train, features_test, labels_train, labels_test = preprocess("../tools/word_data_unix.pkl","../tools/email_authors.pkl",j)
+    loop = [0,1,2,3,4,5,6,7,8,9]
+    print("Test sample Size:",features_train.size)
+    ##Loop to change the var_smoothing
+    for i in loop:
+        num=1/(10)**i
+        gnb = GaussianNB(var_smoothing=num)
+        time0=time.time()
+        pred = gnb.fit(features_train, labels_train).predict(features_test)
+        time1=time.time()
+        acc=accuracy_score(labels_test,pred)
+        print("Test sample_Size: ",j," Accuracy for ",num,": ", acc,"Ellapsed time: ",time1-time0)
+        i=i+1
+    j=1+1
 #########################################################
 
 
